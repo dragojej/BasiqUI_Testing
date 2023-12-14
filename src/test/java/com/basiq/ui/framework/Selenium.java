d19845da49bb417b7eba9b7884ac906b3abb4878
dragojej <dragoje.janjevic@basiq.io>
2023-12-14 07:51:35 +0100

    Added Selenium.

---
+++ src/test/java/com/basiq/ui/framework/Selenium.java
@@ -0,0 +1,28 @@
+package com.basiq.ui.framework;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class Selenium {
+
+    @Test
+    public void openChrome() {
+        WebDriverManager.chromedriver().setup();
+        String driverPath = System.getProperty("user.dir") + "/src/main/java/drivers/chromedriver";
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("--remote-allow-origins=*");
+        ChromeDriver driver = new ChromeDriver(options);
+        //driver.manage().addCookie(ck);
+        //driver.get(authorizationUrl);
+
+        driver.get("https://google.com");
+        driver.findElement(By.name("q")).sendKeys("Selenium");
+        driver.quit();
+    }
+
+}
